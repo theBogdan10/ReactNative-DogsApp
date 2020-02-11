c059a76b871a081a80f0da720157f565c36aa01b
Bogdan <bybogdan@mail.ru>
2020-02-11 16:30:52 +0300

    fixed null is not an object(evaluating RNI18n.languages)

--- src/i18.js
+++ src/i18.js
@@ -1,8 +1,7 @@
 
-import I18n from 'react-native-i18n';
+import I18n from './index';
 import * as en from './en.json';
 import * as ar from './ar.json';
-
 
 I18n.defaultLocale = 'en';
 I18n.fallbacks = true;
